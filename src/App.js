b3f9543f5463764c009b65a408c4dbc6d660c540
mahyar1982 <mahyaar.mohammadi@gmail.com>
2018-07-11 21:04:18 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'; 
 import Main from './components/main';
-import { Link } from 'react-router-dom';
+import { Link, HashRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
         <Layout>
-    <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">My Portfolio</Link>} scroll>
+    <Header className="header-color" title={<Link exact style={{textDecoration: 'none', color: 'white'}} to="/">My Portfolio</Link>} scroll>
+              
               <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
+                <Link exact to="/resume">Resume</Link>
+                <Link exact to="/projects">Projects</Link>
+                <Link exact to="/contact">Contact</Link>
               </Navigation>
             </Header>
-            <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">My Portfolio</Link>}>
+            <Drawer title={<Link exact style={{textDecoration: 'none', color: 'black'}} to="/">My Portfolio</Link>}>
               <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
+                <Link exact to="/resume">Resume</Link>
+                <Link exact to="/projects">Projects</Link>
+                <Link exact to="/contact">Contact</Link>
               </Navigation>
             </Drawer>
           <Content>
